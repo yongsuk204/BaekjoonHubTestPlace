0f4d110a2d27d4b52ae59527628e5950a8246a14
yongsuk204 <yongsuk204@gmail.com>
2025-01-20 00:16:53 +0900

    0120

--- .vscode/solve.py
+++ .vscode/solve.py
@@ -1,29 +1,15 @@
-# all_grade = [0 for i in range(20)]      # 입력값 넣을 빈 리스트 만들기
-# all_score =  {
-#     "A+": 4.5,
-#     "A0": 4.0,
-#     "B+": 3.5,
-#     "B0": 3.0,
-#     "C+": 2.5,
-#     "C0": 2.0,
-#     "D+": 1.5,
-#     "D0": 1.0,
-#     "F": 0.0
-# }
-# total_score = 0
-# for i in range(20):
-#     all_grade[i] = list(map(str, input().split()))      # 입력값을 리스트로 만들기
-
-
-
-a = int(input())
-if a >= 90:
-    print('A')
-elif a >= 80:
-    print('B')
-elif a >= 70:
-    print('C')
-elif a >= 60:
-    print('D')
-elif a <  60:
-    print('F')
+all_grade = [0 for i in range(20)]      # 입력값 넣을 빈 리스트 만들기
+all_score =  {
+    "A+": 4.5,
+    "A0": 4.0,
+    "B+": 3.5,
+    "B0": 3.0,
+    "C+": 2.5,
+    "C0": 2.0,
+    "D+": 1.5,
+    "D0": 1.0,
+    "F": 0.0
+}
+total_score = 0
+for i in range(20):
+    all_grade[i] = list(map(str, input().split()))      # 입력값을 리스트로 만들기
