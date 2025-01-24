83fdf48888eae3f95cd6aed5c4216b807e3f672e
yongsuk204 <yongsuk204@gmail.com>
2025-01-25 01:44:07 +0900

    [Bronze I] Title: 세로읽기, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/10798. 세로읽기/세로읽기.py
@@ -0,0 +1,14 @@
+words = []      # 입력을 받을 리스트생성
+
+for _ in range(5):      # 5개 입력 받아서 하나의 리스트에 각각 스트링으로 담아둠
+    words.append(input())
+
+result = ''     # 세로로 읽은것을 담을 객체생성
+
+for i in range(15):     # 입력값 가로가 1~15개
+    for j in range(5):      # 입력값은 무조건 5개(줄)이다
+        if i < len(words[j]):   # 가로입력길이가 15이하인것만 반영하겠다는 조건
+            result += words[j][i]
+            # out of range가 일어날 수 없는 이유는 If 조건문에서 걸러진다.
+
+print(result)
