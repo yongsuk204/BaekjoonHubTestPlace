1fe2420f8658d2b1bbe6241e6653ba867448b076
yongsuk204 <yongsuk204@gmail.com>
2025-01-30 12:11:01 +0900

    백준 5543

--- .vscode/solve.py
+++ .vscode/solve.py
@@ -1,2 +1,14 @@
-R1, S = map(int, input().split())
-print((2*S)-R1)
+menu_price_list = []
+drink_price_list = []
+set_total_price_list = []
+
+for i in range(3):
+    menu_price_list.append(int(input()))
+
+for i in range(2):
+    drink_price_list.append(int(input()))
+
+for i in menu_price_list:
+    for f in drink_price_list:
+        set_total_price_list.append(i+f-50)
+print(min(set_total_price_list))
