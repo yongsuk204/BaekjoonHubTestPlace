aa7e4cba39e7d6f941e87aa51090aa121bd99f07
yongsuk204 <yongsuk204@gmail.com>
2025-02-07 22:26:46 +0900

    Update 에라토스테네스의 체.py

--- 백준/Silver/2960. 에라토스테네스의 체/에라토스테네스의 체.py
+++ 백준/Silver/2960. 에라토스테네스의 체/에라토스테네스의 체.py
@@ -7,7 +7,7 @@ for i in range(2, a+1):     # 2~10까지 반복
     # print(i)
     if empty_list[i]:   # 해당숫자가 소수라면
         for f in range(i,a+1,i):    # 소수인 숫자부터 입력숫자까지 소수의배수들
-            if empty_list[f]:   # 소수인 숫자 포함 소수의 배수들이 있다면
+            if empty_list[f]:   # 소수인 숫자 포함 소수의 배수들이 있다면 / f -> i 로해서 실수했었는데, empty_list[i]로 고정되어버리기 때문에 소수의 배수들이 지워지지 않고 하위반복문이 끝나서 상위반복문으로 들어감, 즉 empty_list[i]는 계속True로 판정하게됨
                 # print(f)
                 empty_list[f] = False   # 그것들 다 제외
                 count += 1      # 제외할때마다 카운트
