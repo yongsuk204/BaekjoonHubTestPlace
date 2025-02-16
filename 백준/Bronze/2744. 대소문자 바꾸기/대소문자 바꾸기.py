d71c3fe8d0c669e5b1735f467a4c4eff2142be33
yongsuk204 <yongsuk204@gmail.com>
2025-02-16 14:15:46 +0900

    [Bronze V] Title: 대소문자 바꾸기, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/2744. 대소문자 바꾸기/대소문자 바꾸기.py
@@ -0,0 +1,13 @@
+original_word = input()
+# change_word = ''
+# for i in original_word:
+#     # print(i)
+#     if i.isupper():
+#         # print('upper')
+#         change_word += i.lower()
+#     else:
+#         # print('lower')
+#         change_word += i.upper()
+# print(change_word)
+
+print(original_word.swapcase())
