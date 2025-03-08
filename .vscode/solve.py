da4663e562e03e5f69e776fd38ae61a32fb09440
yongsuk204 <yongsuk204@gmail.com>
2025-03-08 10:54:39 +0900

    백준 문제풀이

--- .vscode/solve.py
+++ .vscode/solve.py
@@ -1,2 +1,11 @@
 import sys
-print(sys.stdin.read())  # 입력된 전체 데이터를 한 번에 읽어서 출력
+a = int(sys.stdin.readline())
+
+ampty_list = [i for i in range(1, a+1)]
+# for i in range(a):
+#     ampty_list.append(i+1)
+
+while ampty_list:
+    print(ampty_list.pop(0))
+    if ampty_list:
+        ampty_list.append(ampty_list.pop(0))
