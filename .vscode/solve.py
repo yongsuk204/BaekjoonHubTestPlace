39b9418e0c2865ac56f22235906b250fffd81b61
yongsuk204 <yongsuk204@gmail.com>
2025-03-11 09:08:18 +0900

    문제풀이

--- .vscode/solve.py
+++ .vscode/solve.py
@@ -2,12 +2,11 @@ from collections import deque
 import sys
 
 a, b = map(int, sys.stdin.readline().split())
+list_1 = deque([i for i in range(1, a+1)])
+ampty_list = []
 
-deque_1 = deque([i for i in range(1,a+1)])
-
-total_list=[]
-
-while deque_1:
-    deque_1.rotate(-(b-1))
-    total_list.append(deque_1.popleft())
-print("<" + ", ".join(map(str, total_list)) + ">")  # 리스트를 요소사이에 ", "를 넣은 문자열로 변환 하고 양옆에 < 와 > 를 문자열로 더함
+while list_1:
+    list_1.rotate(-(b-1))
+    ampty_list.append(list_1.popleft())
+# print(ampty_list)
+print("<"+", ".join(map(str, ampty_list))+">")
