d679c9b9b76fabea9027d2686cb4c74ee88a03fb
yongsuk204 <yongsuk204@gmail.com>
2025-03-12 22:02:33 +0900

    백준 solve 확장

--- .vscode/solve.py
+++ .vscode/solve.py
@@ -1,12 +1,48 @@
-from collections import deque
 import sys
+from collections import deque
 
-a, b = map(int, sys.stdin.readline().split())
-list_1 = deque([i for i in range(1, a+1)])
-ampty_list = []
+input_time = int(sys.stdin.readline())
 
-while list_1:
-    list_1.rotate(-(b-1))
-    ampty_list.append(list_1.popleft())
-# print(ampty_list)
-print("<"+", ".join(map(str, ampty_list))+">")
+list_1 = deque()
+
+def push(x):
+    list_1.append(x)
+def pop():
+    if not list_1:
+        return -1
+    else:
+        return list_1.popleft()
+def size():
+    return len(list_1)
+def empty():
+    if not list_1:
+        return 1
+    else:
+        return 0
+def front():
+    if not list_1:
+        return -1
+    else:
+        return list_1[0]
+def back():
+    if not list_1:
+        return -1
+    else:
+        return list_1[-1]
+    
+for i in range(input_time):
+    command = sys.stdin.readline().split()
+    if command[0] == 'push':
+        push(command[1])
+    elif command[0] == 'pop':
+        print(pop())
+    elif command[0] == 'size':
+        print(size())
+    elif command[0] == 'empty':
+        print(empty())
+    elif command[0] == 'front':
+        print(front())
+    elif command[0] == 'back':
+        print(back())
+    else:
+        break
