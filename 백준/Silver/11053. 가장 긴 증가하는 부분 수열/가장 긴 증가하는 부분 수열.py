42c3c05c04c662e269381b0c5ce68d03d024b490
yongsuk204 <yongsuk204@gmail.com>
2025-03-16 20:15:44 +0900

    [Silver II] Title: 가장 긴 증가하는 부분 수열, Time: 140 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.py
@@ -0,0 +1,19 @@
+import sys
+
+# 입력 받기
+n = int(sys.stdin.readline())
+arr = list(map(int, sys.stdin.readline().split()))
+
+# DP 테이블 초기화
+dp = [1] * n
+
+# LIS 계산
+for i in range(1, n):
+    # print(i)
+    for j in range(i):
+        if arr[j] < arr[i]:  # 증가하는 경우
+            dp[i] = max(dp[i], dp[j] + 1)
+
+
+# 가장 긴 부분 수열의 길이 출력
+print(max(dp))
