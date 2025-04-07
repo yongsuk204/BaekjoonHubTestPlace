b2b52ef07c82eb420006ce3cf1056de19cc5444b
yongsuk204 <yongsuk204@gmail.com>
2025-04-07 23:53:36 +0900

    11111

--- .vscode/solve.py
+++ .vscode/solve.py
@@ -1,14 +1,11 @@
-import sys
+total_list = [[0] * 100 for i in range(100)]
 
-dp = [1,1,1] + [None] * 36
+what_time = int(input())
+for i in range(what_time):
+    a, b = map(int, input().split())
+    for j in range(a-1, a+9):
+        for k in range(b-1, b+9):
+            total_list[j][k] = 1
 
-def fib(n):
-    if dp[n]:
-        return dp[n]
-    else:
-        dp[n] = fib(n - 1) + fib(n - 2)
-        return  dp[n]
+print(sum(map(sum,total_list)))
     
-
-print(fib(30))
-print(dp)
